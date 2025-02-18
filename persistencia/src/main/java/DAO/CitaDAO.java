17396f9a2c1afeda9d8d938f00ffb9a4e5cc15d9
Ernesto López <jesus.lopez252663@potros.itson.edu.mx>
2025-02-18 16:47:39 -0700

    PacienteDAO y Creación de ICitaDAO y CitaDAO
    Se eliminan algunos comentarios por default de netbeans en PacienteDAO y se crea la plantilla para los metodos de la CitaDAO

---
+++ persistencia/src/main/java/DAO/CitaDAO.java
@@ -0,0 +1,30 @@
+package DAO;
+
+import Exception.PersistenciaException;
+import entidades.Cita;
+
+/**
+ *
+ * @author ErnestoLpz_252663
+ */
+public class CitaDAO {
+    public Cita agendarCita() throws PersistenciaException{
+        
+    }
+    
+    public Cita agendarCitaEmergencia() throws PersistenciaException{
+        
+    }
+    
+    public Cita desagendarCita() throws PersistenciaException{
+        
+    }
+    
+    public Cita AuditoriaCita()throws PersistenciaException{
+        
+    }
+    
+    public Cita AuditoriaCitaEmergencia() throws PersistenciaException{
+        
+    }
+}
---
+++ persistencia/src/main/java/DAO/ICitaDAO.java
@@ -0,0 +1,35 @@
+package DAO;
+
+import Exception.PersistenciaException;
+import entidades.Cita;
+
+/**
+ *
+ * @author ErnestoLpz_252663
+ */
+public interface ICitaDAO {
+    /**
+     * Agenda una cita normal (no de emergencia).
+     */
+    public Cita agendarCita() throws PersistenciaException;
+
+    /**
+     * Agenda una cita de emergencia.
+     */
+    public Cita agendarCitaEmergencia() throws PersistenciaException;
+
+    /**
+     * Desagenda una cita existente
+     */
+    public Cita desagendarCita() throws PersistenciaException;
+
+    /**
+     * Realiza una auditoría de una cita normal (no de emergencia)
+     */
+    public Cita AuditoriaCita() throws PersistenciaException;
+
+    /**
+     * Realiza una auditoría de una cita de emergencia
+     */
+    public Cita AuditoriaCitaEmergencia() throws PersistenciaException;
+}
