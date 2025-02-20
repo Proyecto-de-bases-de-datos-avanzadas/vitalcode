3634c67e24dc3df2112f1da340e2d7aaa7cfcc49
AlexNieblas10 <elalexnieblas@gmail.com>
2025-02-20 07:55:26 -0700

    Pruebas de negocio añadidas

--- negocio/src/main/java/com/mycompany/negocio/Negocio.java
+++ negocio/src/main/java/com/mycompany/negocio/Negocio.java
@@ -4,13 +4,25 @@
 
 package com.mycompany.negocio;
 
+import DTO.PacienteNDTO;
+import DTO.UsuarioNDTO;
+import configuracion.DependencyInjector;
+import java.sql.Date;
+import Exception.NegocioException;
+import Exception.PersistenciaException;
 /**
  *
  * @author alexnieblas
  */
 public class Negocio {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws PersistenciaException {
+        PacienteNDTO pacienteAregistrar = new PacienteNDTO("loquesea@gmail.com", "Alex", "Nieblas", "Moreno", "6442253326", Date.valueOf("2005-08-12"));
+        UsuarioNDTO usuarioARegistrar = new UsuarioNDTO("LOQUE", "otra", "Paciente");
+        try{
+            DependencyInjector.crearPacienteBO().registrarPaciente(usuarioARegistrar, pacienteAregistrar);
+        }catch(NegocioException ne){
+            ne.printStackTrace();
+        }
     }
 }
