6fb536a7ed776728deac118eaf2369f507a47a5b
AlexNieblas10 <elalexnieblas@gmail.com>
2025-02-20 06:32:37 -0700

    DTO de usuario agregado

---
+++ negocio/src/main/java/DTO/UsuarioNDTO.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author alexnieblas
+ */
+public class UsuarioNDTO {
+    private String nombre_usuario; 
+    private String contraseniaUsuario; 
+    private String tipo_usuario;
+
+    public UsuarioNDTO() {
+    }
+
+    public UsuarioNDTO(String nombre_usuario, String contraseniaUsuario, String tipo_usuario) {
+        this.nombre_usuario = nombre_usuario;
+        this.contraseniaUsuario = contraseniaUsuario;
+        this.tipo_usuario = tipo_usuario;
+    }
+
+    public String getNombre_usuario() {
+        return nombre_usuario;
+    }
+
+    public void setNombre_usuario(String nombre_usuario) {
+        this.nombre_usuario = nombre_usuario;
+    }
+
+    public String getContraseniaUsuario() {
+        return contraseniaUsuario;
+    }
+
+    public void setContraseniaUsuario(String contraseniaUsuario) {
+        this.contraseniaUsuario = contraseniaUsuario;
+    }
+
+    public String getTipo_usuario() {
+        return tipo_usuario;
+    }
+
+    public void setTipo_usuario(String tipo_usuario) {
+        this.tipo_usuario = tipo_usuario;
+    }
+}
